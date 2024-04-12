9a7b4e2e64ed14529834fb0d6653f654ea23c8c3
c2coder <jansky.stepan08@gmail.com>
2024-04-12 21:39:31 +0200

    Working code

--- converter.py
+++ converter.py
@@ -1,7 +1,7 @@
 import sys
-import PIL
+from PIL import Image, ImageSequence
 
-raw_data:bytes
+raw_data: bytes
 out = []
 
 in_file = sys.argv[1]
@@ -9,8 +9,23 @@ out_file = sys.argv[2]
 gif_name = sys.argv[3]
 
 
-with open(in_file, "rb") as f:
-    raw_data=f.read()
+def resize(frames):
+    for frame in frames:
+        f = frame.copy()
+        yield f.resize((240, 240))
+
+
+im = Image.open(in_file)
+frames = ImageSequence.Iterator(im)
+
+frames = resize(frames)
+
+om = next(frames)
+om.info = im.info
+om.save(f"edited_{in_file}", save_all=True, append_images=list(frames))
+
+with open(f"edited_{in_file}", "rb") as f:
+    raw_data = f.read()
 
 
 for i, b in enumerate(raw_data):
@@ -23,7 +38,7 @@ with open(out_file, "w") as f:
     for i in range(0, len(out), 8):
 
         # Join 8 strings with a space separator
-        line = ', '.join(out[i:i+8]).strip(", ")+ ",\n"
+        line = ', '.join(out[i:i+8]).strip(", ") + ",\n"
         # Write the line to the file
         f.write(line)
 
